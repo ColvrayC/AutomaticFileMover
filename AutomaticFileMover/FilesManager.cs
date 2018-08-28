46bf806a63e2669391e586efb9e133900d74d5cd
Unknown <camille.colvray@ynov.com>
2018-08-28 17:58:10 +0200

    add waiter to 30sec

--- AutomaticFileMover/FilesManager.cs
+++ AutomaticFileMover/FilesManager.cs
@@ -26,7 +26,7 @@ namespace AutomaticFileMover
 
         private void bw_DoSleep(object sender, DoWorkEventArgs e)
         {
-                Thread.Sleep(5000);
+                Thread.Sleep(30000);
         }
 
         /// <summary>
